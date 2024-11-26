9e5901270fb6418b8478a73f41c1ddd6b2def2f5
MehdiMahmoudico <mmeehdi.mma@gmail.com>
2024-11-26 15:17:52 +0100

    Fixed the war file build issue

--- server/src/main/java/com/example/demo/TravelApplication.java
+++ server/src/main/java/com/example/demo/TravelApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -12,5 +16,16 @@ public class TravelApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TravelApplication.class, args);
 	}
-
+	 @Bean
+	    public TomcatServletWebServerFactory servletContainer() {
+	        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+	        Connector ajpConnector = new Connector("AJP/1.3");
+	        ajpConnector.setPort(9090);
+	        ajpConnector.setSecure(false);
+	        ajpConnector.setAllowTrace(false);
+	        ajpConnector.setScheme("http");
+		((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
+	        tomcat.addAdditionalTomcatConnectors(ajpConnector);
+	        return tomcat;
+	    }
 }
